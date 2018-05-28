1ad1a1f09dbb58e505b4c90bcd06b4c5d8104134
Larry Botha <larry@fixate.it>
2018-05-28 22:48:41 +0200

    test(added testing): added jest and skipped tests

---
+++ _tests__/index.test.js
@@ -0,0 +1,9 @@
+describe('store', () => {
+  test.skip('-> state stream receives broadcast events', () => {});
+
+  test.skip('-> state stream is updated after reacting to broadcast event', () => {});
+
+  test.skip('-> action stream receives broadcast events', () => {});
+
+  test.skip('-> side effects receive broadcast events', () => {});
+});
