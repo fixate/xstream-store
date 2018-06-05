cd69fb0c512301224da6ae3c13a46324dfc7d548
Larry Botha <larry@fixate.it>
2018-06-05 09:27:03 +0200

    docs(examples): fix missing example

--- examples/index.ts
+++
@@ -1,4 +0,0 @@
-const xs = require('xstream')
-const createStore = require('xstream-store')
-
-
---
+++ examples/store.js
@@ -0,0 +1,22 @@
+const createStore = require('../src/index').default;
+const {counter$Creator, counterEffectsCreator, add, increment} = require('./counter');
+
+const streamCreators = {counter: counter$Creator};
+const effectCreators = [counterEffectsCreator];
+
+const store = createStore(streamCreators, effectCreators);
+
+store.state$
+  // get only the counter state
+  .map(({counter}) => counter)
+  .addListener({
+    next(counterState) {
+      console.log(counterState);
+    },
+  });
+
+// add 1 to counter state
+store.dispatch(add(1));
+
+// then increment counter state
+store.dispatch(increment());
